1b145110ab164ae3d34a8d3e7387175e856d4fee
Marcell <marcell.dechant@neuefische.de>
2023-11-09 10:11:05 +0100

    Delete BackendApplicationTests

--- backend/src/main/java/com/example/backend/security/AppUser.java
+++ backend/src/main/java/com/example/backend/security/AppUser.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
